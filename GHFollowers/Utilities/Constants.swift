f81d36b1b97088b314f273c1dc91254e9a74818b
Doğan Ensar Papuçcuoğlu <ensarpabuccuoglu@gmail.com>
2024-12-10 00:32:16 +0300

    Update UserInfoVC UI

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Doğan Ensar Papuçcuoğlu on 10.12.2024.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
