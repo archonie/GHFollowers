23f59b8f1502f59566b34b87d6d6253d3f5acba4
Doğan Ensar Papuçcuoğlu <ensarpabuccuoglu@gmail.com>
2024-12-09 18:33:46 +0300

    Update Search functionality

--- GHFollowers/Custom Views/Views/GFEmptyStateView.swift
+++ GHFollowers/Custom Views/Views/GFEmptyStateView.swift
@@ -46,8 +46,8 @@ class GFEmptyStateView: UIView {
             
             logoImageView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3),
             logoImageView.heightAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3),
-            logoImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 170),
-            logoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 40)
+            logoImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 210),
+            logoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 55)
         ])
     }
     
--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -14,6 +14,7 @@ class FollowerListVC: UIViewController {
     }
 
     private var followers: [Follower] = []
+    private var filteredFollowers: [Follower] = []
     var username: String!
     private var page: Int = 1
     private var hasMoreFollowers = true
@@ -25,6 +26,7 @@ class FollowerListVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewController()
+        configureSearchController()
         configureCollectionView()
         getFollowers(username: username, page: page)
         configureDataSource()
@@ -58,7 +60,7 @@ class FollowerListVC: UIViewController {
                     }
                     return
                 }
-                self.updateData()
+                self.updateData(on: self.followers)
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Error", message: error.rawValue, buttonTitle: "Ok")
             }
@@ -66,9 +68,9 @@ class FollowerListVC: UIViewController {
     }
     
     private func configureViewController() {
-        
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.showsLargeContentViewer = true
     }
     
     private func configureCollectionView() {
@@ -79,7 +81,16 @@ class FollowerListVC: UIViewController {
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
     }
     
-    
+    private func configureSearchController() {
+        let searchController = UISearchController()
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
+        searchController.searchBar.placeholder = "Search for a username"
+        searchController.obscuresBackgroundDuringPresentation = false
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
+        navigationItem.hidesSearchBarWhenScrolling = false
+    }
     
     private func configureDataSource() {
         dataSoure = UICollectionViewDiffableDataSource<Section, Follower>(collectionView: collectionView, cellProvider: { collectionView, indexPath, follower in
@@ -89,7 +100,7 @@ class FollowerListVC: UIViewController {
         })
     }
     
-    private func updateData() {
+    private func updateData(on followers: [Follower]) {
         var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
         snapshot.appendSections([.main])
         snapshot.appendItems(followers)
@@ -118,7 +129,26 @@ extension FollowerListVC: UICollectionViewDelegate {
         
     }
     
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        navigationItem.hidesSearchBarWhenScrolling = true
+    }
+    
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         collectionView.deselectItem(at: indexPath, animated: true)
+    }
+}
+
+extension FollowerListVC: UISearchResultsUpdating, UISearchBarDelegate {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let filter = searchController.searchBar.text, !filter.isEmpty else {
+            return
+        }
+        
+        filteredFollowers = followers.filter({ $0.login.lowercased().contains(filter.lowercased())})
+        updateData(on: filteredFollowers)
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        updateData(on: followers)
     }
 }
